a0e1357ca9719bec0b97e4074269c88d1bd43493
Fnac Beaugrenelle <fnacbeaugrenelle@MacBook-Pro-de-Fnac.local>
2023-05-20 16:24:43 +0200

    ajout route pour la page admin

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/Home";
 import Offers from "./pages/Offers/Offers";
+import Administrator from "./pages/Administrator/Administrator";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/offers" element={<Offers />} />
+        <Route path="/administrator_dashboard" element={<Administrator />} />
       </Routes>
     </div>
   );
