6587d0d0a4a38f52713a80a57b36d915bb9ce5d7
Pierro236 <pierresedo@gmail.com>
2023-05-21 15:03:48 +0200

    fix: dashboard

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./Sidebar.module.css";
 
 const Sidebar = () => {
   return (
     <div className={styles.sidebar}>
-      <h4>Dashboard</h4>  
+      <h4>Dashboard</h4>
       <ul>
         <li>
-          <Link to="/admin/users" className={styles.sidebar-link}>
+          <Link to="/admin/users" className={styles.sidebarLink}>
             Gestion des utilisteurs
           </Link>
         </li>
         <li>
-          <Link to="/admin/mailbox" className={styles.sidebar-link}>
+          <Link to="/admin/mailbox" className={styles.sidebarLink}>
             Messagerie
           </Link>
         </li>
         <li>
-          <Link to="/admin/advertisement" className={styles.sidebar-link}>
+          <Link to="/admin/advertisement" className={styles.sidebarLink}>
             Gestion des Annonces
           </Link>
         </li>
         <li>
-          <Link to="/admin/faq" className={styles.sidebar-link}>
+          <Link to="/admin/faq" className={styles.sidebarLink}>
             Mise à jour de la FAQ
           </Link>
         </li>
--- src/pages/Administrator/Administrator.js
+++ src/pages/Administrator/Administrator.js
@@ -1,18 +1,18 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
-import Header from "../../components/Header/Header";
 import Sidebar from "../../components/Sidebar/Sidebar";
+import styles from "./Administrator.module.css";
 
 const Administrator = () => {
   return (
-    
-    <div className="admin-page">
-        <Header />
-        <Navbar />
+    <div>
+      <Navbar />
+      <div className={styles.adminPage}>
         <Sidebar />
-        <div className="admin-content">
-            <p>Welcome to Administrator Page !</p>
+        <div className={styles.adminContent}>
+          <p>Welcome to Administrator Page !</p>
         </div>
+      </div>
     </div>
   );
 };
